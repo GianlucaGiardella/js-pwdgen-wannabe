f6bb1e30d57cf72ab5f7ccf4082f66a76aa23893
GianluGiarde <gianlugiarde@gmail.com>
2023-04-05 09:50:06 +0200

    fix: const js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const firstName = prompt('What\'s your first name ?');
-const lastName = prompt('What\'s your first last name ?');
-const favColor = prompt('What\'s your favorite color ?');
+const lastName = prompt('What\'s your last name ?');
+const favoriteColor = prompt('What\'s your favorite color ?');
 
-const newPassword = firstName + lastName + favColor;
+const newPassword = firstName + lastName + favoriteColor;
 
 document.getElementById("generated").innerHTML = `Your new password is: ${newPassword}23`;
